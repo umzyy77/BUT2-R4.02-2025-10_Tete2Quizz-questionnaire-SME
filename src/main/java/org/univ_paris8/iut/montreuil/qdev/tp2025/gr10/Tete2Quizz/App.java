673c69223eea18db3d9fe59bfb358c30313367b1
umar <uhafesji@iut.univ-paris8.fr>
2025-02-13 16:21:35 +0100

    version 1.0.0

---
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr10/Tete2Quizz/App.java
@@ -0,0 +1,10 @@
+package org.univ_paris8.iut.montreuil.qdev.tp2025.gr10.Tete2Quizz;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr10/Tete2Quizz/AppTest.java
@@ -0,0 +1,19 @@
+package org.univ_paris8.iut.montreuil.qdev.tp2025.gr10.Tete2Quizz;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
