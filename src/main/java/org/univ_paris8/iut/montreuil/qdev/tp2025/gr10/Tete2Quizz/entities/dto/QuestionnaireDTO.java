2435dcd4ae15dd234322a63e4ec2aeb507524ff6
Angerenage <ange.renage@gmail.com>
2025-03-26 11:55:01 +0100

    Modification des DTOs et ajout de l'enum des langues

--- src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr10/Tete2Quizz/entities/dto/QuestionnaireDTO.java
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr10/Tete2Quizz/entities/dto/QuestionnaireDTO.java
@@ -1,27 +1,26 @@
 package org.univ_paris8.iut.montreuil.qdev.tp2025.gr10.Tete2Quizz.entities.dto;
 
+import org.univ_paris8.iut.montreuil.qdev.tp2025.gr10.Tete2Quizz.utils.enums.LangEnum;
+import org.univ_paris8.iut.montreuil.qdev.tp2025.gr10.Tete2Quizz.utils.exceptions.load.InvalidIdException;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class QuestionnaireDTO {
     private int id;
-    private String langue;
     private String titre;
+    private LangEnum langue;
     private int nombreQuestions;
     private List<QuestionDTO> questions;
 
-    public QuestionnaireDTO(int id, String langue, String titre, int nombreQuestions, List<QuestionDTO> questions) {
+    public QuestionnaireDTO(int id, LangEnum langue) {
         this.id = id;
         this.langue = langue;
-        this.titre = titre;
-        this.nombreQuestions = nombreQuestions;
-        this.questions = questions;
+        this.nombreQuestions = 0;
+        this.questions = new ArrayList<>();
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public String getLangue() {
+    public LangEnum getLangue() {
         return langue;
     }
 
@@ -35,6 +34,19 @@ public class QuestionnaireDTO {
 
     public List<QuestionDTO> getQuestions() {
         return questions;
+    }
+
+    public void addQuestion(QuestionDTO question) throws InvalidIdException {
+        for (QuestionDTO existing : questions) {
+            if (existing.getIdQuestion() == question.getIdQuestion()) {
+                throw new InvalidIdException(
+                        "Une question avec l'ID " + question.getIdQuestion() + " existe déjà."
+                );
+            }
+        }
+
+        questions.add(question);
+        nombreQuestions++;
     }
 }
 
